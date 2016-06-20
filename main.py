e0ebcaf3638bdb24b1a26ae7caff14ec96886071
Brandon Walsh <walshb@wlu.edu>
2016-06-20 16:29:59 -0400

    refactoring as class. lord. so much easier now.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-from nltk import word_tokenize, FreqDist
+import nltk
+from nltk import word_tokenize, FreqDist, PorterStemmer
 from nltk.corpus import stopwords, names
+from nltk.stem.snowball import SnowballStemmer
 import codecs
 import os
 import drive
@@ -11,100 +13,134 @@ import operator
 import dateutil.parser
 
 
+
+# TODO: Refactor for object oriented principles. really required for stemming.
+# TODO: stemming. will need to follow the example below, where you have each text map onto an indexed stem.
+# TODO: take those stems and then integrate that into everything below.
+
+""" A text should have:
+    filename
+    date
+    journal
+    tokens
+    stem index for each token"""
+
+
 CORPUS = 'clean_ocr'
+STOPWORD_LIST = []
+LIST_OF_NAMES = []
 
 
-def generate_stopwords():
-    """generates stopwords for use in tokenizing"""
-    nltk_stopwords = stopwords.words('french')
-    # stopwords from http://www.ranks.nl/stopwords/french
-    ranks_stopwords = []
-    with codecs.open('french_stopwords.txt', 'r', 'utf8') as f:
-        ranks_stopwords = [x.strip('\n') for x in f]
-    # put custom stopword list here. Could also
-    # read in as a csv file if that's easier.
-    extra_stopwords = []
-    punctuation = ['»', '«', ',', '-', '.', '!',
-                   "\"", '\'' ':', ';', '?', '...']
-    return set(nltk_stopwords + ranks_stopwords +
-               punctuation + extra_stopwords)
+class Corpus(object):
+    """Takes in a list of text objects. and you can then
+    run things on the set as a whole."""
+
+    def __init__(self, corpus_dir=CORPUS):
+        self.corpus_dir = corpus_dir
+        self.stopwords = self.generate_stopwords()
+        self.names_list = self.generate_names_list()
+        self.texts = self.build_corpus()
+
+    def build_corpus(self):
+        """given a corpus directory, make indexed text objects from it"""
+        texts = []
+        for (root, _, files) in os.walk(self.corpus_dir):
+            for fn in files:
+                texts.append(IndexedText(os.path.join(root, fn)))
+        return texts
+
+    def generate_stopwords(self):
+        global STOPWORD_LIST
+        """generates stopwords for use in tokenizing"""
+        nltk_stopwords = stopwords.words('french')
+        # stopwords from http://www.ranks.nl/stopwords/french
+        ranks_stopwords = []
+        with codecs.open('french_stopwords.txt', 'r', 'utf8') as f:
+            ranks_stopwords = [x.strip('\n') for x in f]
+        # put custom stopword list here. Could also
+        # read in as a csv file if that's easier.
+        extra_stopwords = []
+        punctuation = ['»', '«', ',', '-', '.', '!',
+                       "\"", '\'' ':', ';', '?', '...']
+        STOPWORD_LIST = set(nltk_stopwords + ranks_stopwords +
+                            punctuation + extra_stopwords)
+        return STOPWORD_LIST
+
+    def generate_names_list(self):
+        global LIST_OF_NAMES
+        """puts together a list to query the corpus by"""
+        nltk_names = names.words()
+        # put custom name list here.
+        extra_names = ['Alexandre', 'Steinheil']
+        LIST_OF_NAMES = [w.lower() for w in nltk_names] + [w.lower() for w in extra_names]
+        return LIST_OF_NAMES
 
 
-def names_list():
-    """puts together a list to query the corpus by"""
-    nltk_names = names.words()
-    # put custom name list here.
-    extra_names = ['Alexandre', 'Steinheil']
-    return [w.lower() for w in nltk_names] + [w.lower() for w in extra_names]
+class IndexedText(object):
+    """Text object"""
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.filename = re.sub(r'^.*/|_clean.txt', '', filepath).lower()
+        self.text = self.read_text()
+        self.tokens = [w.lower() for w in word_tokenize(self.text)]
+        self.length = len(self.tokens)
+        self.fq = FreqDist(self.tokens)
+        self.date = self.parse_dates()
+        self.year = dateutil.parser.parse(self.date).year
+        self.month = dateutil.parser.parse(self.date).month
+        self.day = dateutil.parser.parse(self.date).day
+        self.publication = self.parse_publication()
+        self.stemmer = SnowballStemmer('french')
+        self.index = nltk.Index((self.stem(word), i)
+                         for (i, word) in enumerate(self.tokens))
+        self.tokens_without_stopwords = self.remove_stopwords()
 
+    def read_text(self):
+        """given a filename read in the text."""
+        with codecs.open(self.filepath, 'r', 'utf8') as f:
+            return f.read()
 
-LIST_OF_NAMES = names_list()
+    def parse_publication(self):
+        """parse the filename for the publication."""
+        date_pattern = r'[jJ]anuary[a-zA-Z0-9_]*|[fF]ebruary[a-zA-Z0-9_]*|[mM]arch[a-zA-Z0-9_]*|[aA]pril[a-zA-Z0-9_]*|[mM]ay[a-zA-Z0-9_]*|[jJ]une[a-zA-Z0-9_]*|[jJ]uly[a-zA-Z0-9_]*|[aA]ugust[a-zA-Z0-9_]*|[sS]eptember[a-zA-Z0-9_]*|[oO]ctober[a-zA-Z0-9_]*|[nN]ovember[a-zA-Z0-9_]*|[dD]ecember[a-zA-Z0-9_]*'
+        # strip out date from filename and pop off the trailing underscore.
+        return re.sub(date_pattern, '', self.filename)[:-1]
 
+    def parse_dates(self):
+        """parse the filename for the dates."""
+        date_pattern = r'[jJ]anuary[a-zA-Z0-9_]*|[fF]ebruary[a-zA-Z0-9_]*|[mM]arch[a-zA-Z0-9_]*|[aA]pril[a-zA-Z0-9_]*|[mM]ay[a-zA-Z0-9_]*|[jJ]une[a-zA-Z0-9_]*|[jJ]uly[a-zA-Z0-9_]*|[aA]ugust[a-zA-Z0-9_]*|[sS]eptember[a-zA-Z0-9_]*|[oO]ctober[a-zA-Z0-9_]*|[nN]ovember[a-zA-Z0-9_]*|[dD]ecember[a-zA-Z0-9_]*'
+        date = re.findall(date_pattern, self.filename)
+        # replaces the underscores with dashes
+        date = dateutil.parser.parse(re.sub(r'_', '-', date[0]))
+        return ('%s-%s-%s' % (date.year, date.month, date.day))
 
-def all_file_names(dirname=CORPUS):
-    """Reads in the files"""
-    for (root, _, files) in os.walk(dirname):
-        for fn in files:
-            yield os.path.join(root, fn)
+    def stem(self, word):
+        return self.stemmer.stem(word).lower()
 
+    def concordance(self, word, width=40):
+        """given a token, produce a word in context view of it."""
+        key = self.stem(word)
+        # words of context
+        wc = int(width / 4)
+        for i in self.index[key]:
+            lcontext = ' '.join(self.tokens[i - wc:i])
+            rcontext = ' '.join(self.tokens[i:i + wc])
+            ldisplay = '{:>{width}}'.format(lcontext[-width:], width=width)
+            rdisplay = '{:{width}}'.format(rcontext[:width], width=width)
+            print(ldisplay, rdisplay)
 
-def strip_off_file_path(filename):
-    """Takes off extraneous information from the file
-     path and returns the filename alone"""
-    filename = re.sub(r'^.*/|_clean.txt', '', filename)
-    return filename.lower()
+    def remove_stopwords(self):
+        global STOPWORD_LIST
+        """takes a list of tokens and strips out the stopwords"""
+        return [w for w in self.tokens if w.lower() not in STOPWORD_LIST]
 
-
-def read_all_texts(filenames):
-    """Given a list of filenames, read each of them"""
-    for f in filenames:
-        yield (strip_off_file_path(f), read_text(f))
-
-
-def get_articles_metadata(list_of_articles, debug=False):
-    """takes articles in form of [filename, [tokens]],
-    goes out to google drive and gives it the necessary
-    date and time information."""
-    metadata = drive.get_article_metadata()
-    new_list_of_articles = []
-    for article in list_of_articles:
-        for row in metadata:
-            if row['filename'] == article[0]:
-                new_list_of_articles.append({'file_name': article[0],
-                                             'journal': row['newspaper name'],
-                                             'date': row['date'],
-                                             'tokens': tokenize_text(article[1])
-                                             })
-            elif debug:
-                print("********ERROR: FILENAME AND DATE MISMATCH ********")
-                print(row['filename'] + '   ≠   ' + article[0])
-                print("*************")
-            else:
-                pass
-    return new_list_of_articles
-
-
-def read_text(filename):
-    """given a filename read in the text."""
-    with codecs.open(filename, 'r', 'utf8') as f:
-        return f.read()
-
-
-def tokenize_text(file):
-    """Tokenizes the text as is. Strips out page breaks and
-    derniere sections but leaves them as part of the larger text."""
-    return [w.lower() for w in word_tokenize(file)]
-
-
-def remove_stopwords(tokens):
-    """takes a list of tokens and strips out the stopwords"""
-    stopword_list = generate_stopwords()
-    return [w for w in tokens if w.lower() not in stopword_list]
-
-
-def calc_article_length(tokens):
-    """given an article's tokens calculates its length"""
-    return len(tokens)
+    def find_names(self):
+        """creates a frequency distribution of the
+        most common names in the texts"""
+        names_list = LIST_OF_NAMES
+        name_tokens = [w for w in self.tokens if w in names_list]
+        fd = FreqDist(name_tokens)
+        return fd.most_common(50)
 
 
 def count_punctuation(text):
@@ -129,14 +165,6 @@ def most_common(text):
     return fd.most_common(50)
 
 
-def find_names(text, list_of_names=LIST_OF_NAMES):
-    """creates a frequency distribution of the
-    most common names in the texts"""
-    name_tokens = [w for w in text if w in list_of_names]
-    fd = FreqDist(name_tokens)
-    return fd.most_common(50)
-
-
 def read_out(articles):
     """given a series of articles, print out stats for them
     articles are given as a list of tuple pairs (filename, list of tokens)"""
@@ -154,21 +182,15 @@ def read_out(articles):
         output.write("Names: " + str(find_names(article['tokens'])) + '\n')
 
 
-def prepare_all_texts(corpus=CORPUS):
-    """Takes all files from filenames to dict. runs everything in between"""
-    # reads in all filenames from the corpus directory.
-    file_names = list(all_file_names(corpus))
-    # reads in data of texts
-    texts = list(read_all_texts(file_names))
-    # reads in article metadata for texts
-    texts_with_metadata = get_articles_metadata(texts)
-    return texts_with_metadata
-
-
-def parse_dates(date):
-    date = dateutil.parser.parse(date)
-    key = ('%s-%s-%s' % (date.year, date.month, date.day))
-    return key
+# def prepare_all_texts(corpus=CORPUS):
+#     """Takes all files from filenames to dict. runs everything in between"""
+#     # reads in all filenames from the corpus directory.
+#     file_names = list(all_file_names(corpus))
+#     # reads in data of texts
+#     texts = list(read_all_texts(file_names))
+#     # reads in article metadata for texts
+#     texts_with_metadata = get_articles_metadata(texts)
+#     return texts_with_metadata
 
 
 def sort_by_date(articles, token):
@@ -189,9 +211,6 @@ def sort_by_date(articles, token):
             index[key] = (index[key][0] + single_token_count(article['tokens'], token), index[key][1] + len(article['tokens']))
         index['year-month-day'] = token
     return index
-
-# TODO: CSV write out for visualizing
-# TODO: stemming
 
 
 def dict_to_list(a_dict):
@@ -222,12 +241,11 @@ def csv_dump(results_dict):
             csvwriter.writerow(row)
 
 
-
 def main():
     """Main function to be called when the script is called"""
-    text_data = prepare_all_texts()
-    index = sort_by_date(text_data, 'crime')
-    csv_dump(index)
+    corpus = Corpus()
+    # index = sort_by_date(text_data, 'crime')
+    # csv_dump(index)
     # read_out(text_data)
 
 if __name__ == '__main__':
