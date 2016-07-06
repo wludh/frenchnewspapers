2101c101ed7c64c19afdab98847f8e680881358c
Brandon Walsh <walshb@wlu.edu>
2016-07-06 15:38:20 -0400

    deletes file from hilt

--- some_stuff.rb
+++
@@ -1,9 +0,0 @@
-def info(author, date)
-  puts author + " kicked the bucket in " + date +"."
-end
-
-authors = {"Charles Dickens" => "1870", "William Thackeray" => "1863", "Anthony Trollope" => "1882", "Gerard Manley Hopkins" => "1889"}
-
-authors.each do |author,date|
-  info(author,date)
-end
