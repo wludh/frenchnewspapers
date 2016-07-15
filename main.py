a291a273717e8273fb11f09dcc86357a68056e4d
Brandon Walsh <walshb@wlu.edu>
2016-07-15 12:16:09 -0400

    disables pos tagger for now

--- main.py
+++ main.py
@@ -220,9 +220,9 @@ class IndexedText(object):
         self.text = self.read_text()
         self.sentences = self.get_text_sentences()
         self.tokens = self.flatten_sentences()
-        self.tagger = treetaggerwrapper.TreeTagger(TAGLANG='fr', TAGDIR='tagger')
-        self.tree_tagged_tokens = self.get_tree_tagged_tokens()
-        self.tagged_tokens = [(foo.word, foo.pos) for foo in self.tree_tagged_tokens]
+        #self.tagger = treetaggerwrapper.TreeTagger(TAGLANG='fr', TAGDIR='tagger')
+        #self.tree_tagged_tokens = self.get_tree_tagged_tokens()
+        #self.tagged_tokens = [(foo.word, foo.pos) for foo in self.tree_tagged_tokens]
         self.stems = [foo.lemma for foo in self.tree_tagged_tokens]
         self.bigrams = list(nltk.bigrams(self.tokens))
         self.trigrams = list(nltk.trigrams(self.tokens))
