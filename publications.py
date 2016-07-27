289a537f497d6d51ae8d1ffc596baf32c9be365c
Sam Gibson <Sam@Sams-MacBook-Air-2.local>
2016-07-27 13:40:36 -0400

    Adjectives per month by publication

---
+++ publications.py
@@ -0,0 +1,19 @@
+import main
+from nltk import FreqDist
+corpus = main.Corpus()
+publications = {}
+for text in corpus.texts:
+	this_publication = []
+	for token,tag in text.tagged_tokens:
+		if tag == 'ADJ':
+			this_publication.append(token)
+	if text.publication in publications:
+		publications[text.publication]+= this_publication
+	else: 
+		publications[text.publication] = this_publication
+
+print(publications)
+
+for key in publications.keys():
+	publications[key] = FreqDist(publications[key]).most_common(50)
+print(publications)
